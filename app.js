7859a8ab135ca0eafa1a47880f4053211478f99a
ANASS ABOUNOUH <anass.abounouh@gmail.com>
2022-07-28 15:37:12 -0500

    mateus jose

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const Python = require("python-runner");
 
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const result = [];
+const result2 = [];
 
 pythonScript = "/code.py";
+pythonScript2 = "/code2.py";
 
 Python.execScript(__dirname + pythonScript, {
   bin: "python",
@@ -32,6 +37,33 @@ Python.execScript(__dirname + pythonScript, {
 
 app.get("/", function (req, res) {
   res.send(result);
+});
+
+app.post("/", function (req, res) {
+  process.env["TITLE"] = req.body.title;
+
+  console.log("this is our env variable", process.env["TITLE"]);
+  Python.execScript(__dirname + pythonScript2, {
+    bin: "python",
+    args: [req.body.title],
+  })
+    .then(function (data) {
+      console.log("data" + data);
+      fs.createReadStream("./result2.csv")
+        .pipe(csvParser())
+        .on("data", (data) => {
+          result2.push(data);
+        })
+        .on("end", () => {
+          console.log(result2);
+          result2.push(data);
+          res.send(result2);
+        });
+    })
+    .catch(function (err) {
+      console.log("Error", err);
+      res.send(err);
+    });
 });
 
 const server = app.listen(process.env.PORT, function (req, res) {
